299b0f9ce0efeedf17a342fe22ba47566947f473
Mathias <mathias@mondofute.com>
2016-01-28 12:53:52 +0100

    CRM-9 création de la station

---
+++ src/Mondofute/Bundle/GeographieBundle/Form/StationTraductionType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mondofute\Bundle\GeographieBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StationTraductionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle')
+            ->add('enVoiture', TextareaType::class, array('required' => false))
+            ->add('enTrain', TextareaType::class, array('required' => false))
+            ->add('enAvion', TextareaType::class, array('required' => false))
+            ->add('distancesGrandesVilles', TextareaType::class, array('required' => false))
+            ->add('langue', HiddenType::class, array('mapped' => false));;
+
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mondofute\Bundle\GeographieBundle\Entity\StationTraduction'
+        ));
+    }
+}
---
+++ src/Mondofute/Bundle/GeographieBundle/Form/StationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mondofute\Bundle\GeographieBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('codePostal')
+            ->add('moisOuverture')
+            ->add('jourOuverture')
+            ->add('moisFermeture')
+            ->add('jourFermeture')
+            ->add('lienMeteo')
+            ->add('traductions', CollectionType::class, array(
+                'entry_type' => StationTraductionType::class,
+            ))
+            ->add('site', HiddenType::class, array('mapped' => false));
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mondofute\Bundle\GeographieBundle\Entity\Station'
+        ));
+    }
+}
