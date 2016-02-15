5a4427d3639d9ab069f7a856777fc999bfec2ae5
Mathias <mathias@mondofute.com>
2016-02-15 09:45:02 +0100

    correction snowpark mapping

--- src/Mondofute/Bundle/DomaineBundle/Entity/SnowPark.php
+++ src/Mondofute/Bundle/DomaineBundle/Entity/SnowPark.php
@@ -22,5 +22,50 @@ class SnowPark
     {
         return $this->id;
     }
-}
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $traductions;
 
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->traductions = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add traduction
+     *
+     * @param \Mondofute\Bundle\DomaineBundle\Entity\SnowparkTraduction $traduction
+     *
+     * @return SnowPark
+     */
+    public function addTraduction(\Mondofute\Bundle\DomaineBundle\Entity\SnowparkTraduction $traduction)
+    {
+        $this->traductions[] = $traduction;
+
+        return $this;
+    }
+
+    /**
+     * Remove traduction
+     *
+     * @param \Mondofute\Bundle\DomaineBundle\Entity\SnowparkTraduction $traduction
+     */
+    public function removeTraduction(\Mondofute\Bundle\DomaineBundle\Entity\SnowparkTraduction $traduction)
+    {
+        $this->traductions->removeElement($traduction);
+    }
+
+    /**
+     * Get traductions
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTraductions()
+    {
+        return $this->traductions;
+    }
+}
--- src/Mondofute/Bundle/DomaineBundle/Entity/SnowparkTraduction.php
+++ src/Mondofute/Bundle/DomaineBundle/Entity/SnowparkTraduction.php
@@ -22,5 +22,91 @@ class SnowparkTraduction
     {
         return $this->id;
     }
-}
+    /**
+     * @var string
+     */
+    private $description = '';
 
+    /**
+     * @var \Mondofute\Bundle\DomaineBundle\Entity\Snowpark
+     */
+    private $snowpark;
+
+    /**
+     * @var \Mondofute\Bundle\LangueBundle\Entity\Langue
+     */
+    private $langue;
+
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return SnowparkTraduction
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set snowpark
+     *
+     * @param \Mondofute\Bundle\DomaineBundle\Entity\Snowpark $snowpark
+     *
+     * @return SnowparkTraduction
+     */
+    public function setSnowpark(\Mondofute\Bundle\DomaineBundle\Entity\Snowpark $snowpark = null)
+    {
+        $this->snowpark = $snowpark;
+
+        return $this;
+    }
+
+    /**
+     * Get snowpark
+     *
+     * @return \Mondofute\Bundle\DomaineBundle\Entity\Snowpark
+     */
+    public function getSnowpark()
+    {
+        return $this->snowpark;
+    }
+
+    /**
+     * Set langue
+     *
+     * @param \Mondofute\Bundle\LangueBundle\Entity\Langue $langue
+     *
+     * @return SnowparkTraduction
+     */
+    public function setLangue(\Mondofute\Bundle\LangueBundle\Entity\Langue $langue = null)
+    {
+        $this->langue = $langue;
+
+        return $this;
+    }
+
+    /**
+     * Get langue
+     *
+     * @return \Mondofute\Bundle\LangueBundle\Entity\Langue
+     */
+    public function getLangue()
+    {
+        return $this->langue;
+    }
+}
