6dab77ab68fadb515a39befc9cc6dc27aaaebeea
Mathias <mathias@mondofute.com>
2016-02-15 09:42:33 +0100

    correction snowpark mapping

---
+++ src/Mondofute/Bundle/DomaineBundle/Entity/SnowparkTraduction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mondofute\Bundle\DomaineBundle\Entity;
+
+/**
+ * SnowparkTraduction
+ */
+class SnowparkTraduction
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
