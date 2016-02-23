1da33e6a74976271441ed5fd68ed5a95231f8fdb
Mathias <mathias@mondofute.com>
2016-02-23 11:51:32 +0100

    CRM-28 FournisseurBundle fiches fournisseur et creation des entités

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
