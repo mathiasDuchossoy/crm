34b3b79a83d2c3f8e39e05d62404ea70570c0b0a
Mathias <mathias@mondofute.com>
2016-02-24 12:14:38 +0100

    CRM-35 region mise en place du code de duplication

--- src/Mondofute/Bundle/GeographieBundle/Controller/RegionUnifieController.php
+++ src/Mondofute/Bundle/GeographieBundle/Controller/RegionUnifieController.php
@@ -43,7 +43,9 @@ class RegionUnifieController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 //        Liste les sites dans l'ordre d'affichage
-        $sites = $em->getRepository('MondofuteSiteBundle:Site')->chargerSansCrmParClassementAffichage();
+        $sites = $em->getRepository('MondofuteSiteBundle:Site')->findBy(array(), array('classementAffichage' => 'asc'));
+//        $sites = $em->getRepository('MondofuteSiteBundle:Site')->chargerSansCrmParClassementAffichage();
+        $langues = $em->getRepository('MondofuteLangueBundle:Langue')->findAll();
 
         $sitesAEnregistrer = $request->get('sites');
 
@@ -53,13 +55,13 @@ class RegionUnifieController extends Controller
         $this->regionsSortByAffichage($regionUnifie);
 
         $form = $this->createForm('Mondofute\Bundle\GeographieBundle\Form\RegionUnifieType', $regionUnifie);
-        $form->add('submit', SubmitType::class, array('label' => 'Enregistrer'));
+        $form->add('submit', SubmitType::class, array('label' => 'Enregistrer', 'attr' => array('onclick' => 'copieNonPersonnalisable();')));
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $this->supprimerRegions($regionUnifie, $sitesAEnregistrer)
-                ->ajouterCrm($regionUnifie);
+//            $this->supprimerRegions($regionUnifie, $sitesAEnregistrer)
+//                ->ajouterCrm($regionUnifie);
+            $this->supprimerRegions($regionUnifie, $sitesAEnregistrer);
 
 //            $em = $this->getDoctrine()->getManager();
             $em->persist($regionUnifie);
@@ -73,6 +75,7 @@ class RegionUnifieController extends Controller
         return $this->render('@MondofuteGeographie/regionunifie/new.html.twig', array(
             'sitesAEnregistrer' => $sitesAEnregistrer,
             'sites' => $sites,
+            'langues' => $langues,
             'entity' => $regionUnifie,
             'form' => $form->createView(),
         ));
@@ -85,7 +88,8 @@ class RegionUnifieController extends Controller
     private function ajouterRegionsDansForm(RegionUnifie $entity)
     {
         $em = $this->getDoctrine()->getManager();
-        $sites = $em->getRepository('MondofuteSiteBundle:Site')->chargerSansCrmParClassementAffichage();
+//        $sites = $em->getRepository('MondofuteSiteBundle:Site')->chargerSansCrmParClassementAffichage();
+        $sites = $em->getRepository('MondofuteSiteBundle:Site')->findBy(array(), array('classementAffichage' => 'asc'));
         $langues = $em->getRepository('MondofuteLangueBundle:Langue')->findAll();
         foreach ($sites as $site) {
             $siteExiste = false;
@@ -169,34 +173,6 @@ class RegionUnifieController extends Controller
             $traductions = new ArrayCollection(iterator_to_array($iterator));
             $region->setTraductions($traductions);
         }
-    }
-
-    /**
-     * @param RegionUnifie $entity
-     * @return $this
-     */
-    private function ajouterCrm(RegionUnifie $entity)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $siteCrm = $em->getRepository(Site::class)->findOneBy(array('crm' => 1));
-        $regionCrm = null;
-        $classementReferentTmp = 0;
-        $i = 0;
-        // parcourir toute les regions
-        foreach ($entity->getRegions() as $region) {
-            //si i est égal à 0 et que le numéro de classement est inférieur au numéro de classement temporisé
-            if ($i === 0 || $region->getSite()->getClassementReferent() < $classementReferentTmp) {
-                $regionCrm = clone $region;
-                $regionCrm->setSite($siteCrm);
-                $classementReferentTmp = $region->getSite()->getClassementReferent();
-            }
-            $i++;
-        }
-
-        if (!is_null($regionCrm)) {
-            $entity->addRegion($regionCrm);
-        }
-        return $this;
     }
 
     /**
@@ -342,7 +318,9 @@ class RegionUnifieController extends Controller
     public function editAction(Request $request, RegionUnifie $regionUnifie)
     {
         $em = $this->getDoctrine()->getManager();
-        $sites = $em->getRepository('MondofuteSiteBundle:Site')->chargerSansCrmParClassementAffichage();
+//        $sites = $em->getRepository('MondofuteSiteBundle:Site')->chargerSansCrmParClassementAffichage();
+        $sites = $em->getRepository('MondofuteSiteBundle:Site')->findBy(array(), array('classementAffichage' => 'asc'));
+        $langues = $em->getRepository('MondofuteLangueBundle:Langue')->findAll();
 
 //        si request(site) est null nous sommes dans l'affichage de l'edition sinon nous sommes dans l'enregistrement
         $sitesAEnregistrer = array();
@@ -350,9 +328,9 @@ class RegionUnifieController extends Controller
 
 //            récupère les sites ayant la région d'enregistrée
             foreach ($regionUnifie->getRegions() as $region) {
-                if (empty($region->getSite()->getCrm())) {
-                    array_push($sitesAEnregistrer, $region->getSite()->getId());
-                }
+//                if (empty($region->getSite()->getCrm())) {
+                array_push($sitesAEnregistrer, $region->getSite()->getId());
+//                }
             }
         } else {
 
@@ -360,7 +338,7 @@ class RegionUnifieController extends Controller
             $sitesAEnregistrer = $request->get('sites');
         }
 
-        $regionCrm = $this->dissocierRegionCrm($regionUnifie);
+//        $regionCrm = $this->dissocierRegionCrm($regionUnifie);
         $originalRegions = new ArrayCollection();
 //          Créer un ArrayCollection des objets de stations courants dans la base de données
         foreach ($regionUnifie->getRegions() as $region) {
@@ -373,16 +351,18 @@ class RegionUnifieController extends Controller
         $deleteForm = $this->createDeleteForm($regionUnifie);
 
         $editForm = $this->createForm('Mondofute\Bundle\GeographieBundle\Form\RegionUnifieType', $regionUnifie)
-            ->add('submit', SubmitType::class, array('label' => 'Update'));
+            ->add('submit', SubmitType::class, array('label' => 'Mettre à jour', 'attr' => array('onclick' => 'copieNonPersonnalisable();')));
 
         $editForm->handleRequest($request);
 
+
+        // ***** Validation du formulaire *****
         if ($editForm->isSubmitted() && $editForm->isValid()) {
 
             try {
                 $this->supprimerRegions($regionUnifie, $sitesAEnregistrer);
-                $this->mettreAJourRegionCrm($regionUnifie, $regionCrm);
-                $em->persist($regionCrm);
+//                $this->mettreAJourRegionCrm($regionUnifie, $regionCrm);
+//                $em->persist($regionCrm);
                 // Supprimer la relation entre la station et stationUnifie
                 foreach ($originalRegions as $region) {
                     if (!$regionUnifie->getRegions()->contains($region)) {
@@ -424,121 +404,11 @@ class RegionUnifieController extends Controller
         return $this->render('@MondofuteGeographie/regionunifie/edit.html.twig', array(
             'entity' => $regionUnifie,
             'sites' => $sites,
+            'langues' => $langues,
             'sitesAEnregistrer' => $sitesAEnregistrer,
             'form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
         ));
-    }
-
-    /**
-     * retirer la region crm
-     * @param RegionUnifie $entity
-     *
-     * @return mixed
-     */
-    private function dissocierRegionCrm(RegionUnifie $entity)
-    {
-        foreach ($entity->getRegions() as $region) {
-            if ($region->getSite()->getCrm() == 1) {
-//                $station->setStationUnifie(null);
-                $entity->removeRegion($region);
-                return $region;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Mettre à jours ou créer une nouvelle stationCrm (si elle n'existe pas)
-     * Permet aussi la gestion des traductions si elles n'existent pas (notament dans le cas d'un ajout de langue)
-     * Retourne vrai si elle est seulement mise à jours
-     * Retourne faux s'il s'agit d'une nouvelle
-     * @param RegionUnifie $regionUnifie
-     * @param Region $regionCrm
-     * @return bool
-     */
-    private function mettreAJourRegionCrm(RegionUnifie $regionUnifie, Region $regionCrm)
-    {
-        /** @var RegionTraduction $regionTraduc */
-        $em = $this->getDoctrine()->getManager();
-        $tabClassementSiteReferent = array();
-
-//        récupère les classementReferent pour chaque site dans un tableau
-        foreach ($regionUnifie->getRegions() as $region) {
-            $tabClassementSiteReferent[] = $region->getSite()->getClassementReferent();
-        }
-
-        // Récupèrer le site référent dans la base
-        $siteReferent = $em->getRepository(Site::class)->findOneBy(array('classementReferent' => min($tabClassementSiteReferent)));
-
-        $langues = $em->getRepository(Langue::class)->findAll();
-
-        // Parcourir toutes les stations
-        foreach ($regionUnifie->getRegions() as $region) {
-
-            // Si la site de la station est égale au site de référence
-            if ($region->getSite() == $siteReferent) {
-//                dump($region);
-//              ajouter les champs "communs"
-                foreach ($langues as $langue) {
-//                    dump($langue);
-//                    recupere la traduction pour l'entite du site referent
-                    $regionTraduc = $region->getTraductions()->filter(function (RegionTraduction $element) use ($langue) {
-                        return $element->getLangue() == $langue;
-                    })->first();
-
-//                    récupère la traductin dans le crm
-                    $regionTraducCrm = $regionCrm->getTraductions()->filter(function (RegionTraduction $element) use ($langue) {
-                        return $element->getLangue() == $langue;
-                    })->first();
-//                    dump($regionTraduc);
-
-
-//                    null est interdit, si la traduction n'existe pas on passe les attributs a vide
-                    if (is_null($regionTraduc->getLibelle())) {
-                        $regionTraduc->setLibelle('');
-                    }
-                    if (is_null($regionTraduc->getDescription())) {
-                        $regionTraduc->setDescription('');
-                    }
-//                    Si la traduction n'existe pas dans le crm on creer une nouvelle traduction
-                    if (empty($regionTraducCrm)) {
-                        $regionTraducCrm = new RegionTraduction();
-                        $regionTraducCrm->setRegion($regionCrm);
-                        $regionTraducCrm->setLangue($langue);
-//                        dump($regionTraducCrm);
-//                        dump($regionTraduc);
-                        //                    copie les attributs de traduction du site référent dans les traductions du crm
-                        $regionTraducCrm->setLibelle($regionTraduc->getLibelle());
-                        $regionTraducCrm->setDescription($regionTraduc->getDescription());
-                        $regionCrm->addTraduction($regionTraducCrm);
-                    } else {
-                        //                    copie les attributs de traduction du site référent dans les traductions du crm
-                        $regionTraducCrm->setLibelle($regionTraduc->getLibelle());
-                        $regionTraducCrm->setDescription($regionTraduc->getDescription());
-                    }
-
-                }
-            } else {
-
-//                permet de vérifier si la langue existe pour les sites non referents si elle n'existe pas on la rajoute
-                foreach ($langues as $langue) {
-
-//                    recupere la traduction pour la langue $langue
-                    $regionTraduc = $region->getTraductions()->filter(function (RegionTraduction $element) use ($langue) {
-                        return $element->getLangue() == $langue;
-                    })->first();
-
-//                    null est interdit, si la traduction n'existe pas on passe les attributs a vide
-                    if (is_null($regionTraduc->getLibelle())) {
-                        $regionTraduc->setLibelle('');
-                    }
-                    if (is_null($regionTraduc->getDescription())) {
-                        $regionTraduc->setDescription('');
-                    }
-                }
-            }
-        }
     }
 
     /**
@@ -623,5 +493,140 @@ class RegionUnifieController extends Controller
         die;
 
     }
+
+//    /**
+//     * @param RegionUnifie $entity
+//     * @return $this
+//     */
+//    private function ajouterCrm(RegionUnifie $entity)
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//        $siteCrm = $em->getRepository(Site::class)->findOneBy(array('crm' => 1));
+//        $regionCrm = null;
+//        $classementReferentTmp = 0;
+//        $i = 0;
+//        // parcourir toute les regions
+//        foreach ($entity->getRegions() as $region) {
+//            //si i est égal à 0 et que le numéro de classement est inférieur au numéro de classement temporisé
+//            if ($i === 0 || $region->getSite()->getClassementReferent() < $classementReferentTmp) {
+//                $regionCrm = clone $region;
+//                $regionCrm->setSite($siteCrm);
+//                $classementReferentTmp = $region->getSite()->getClassementReferent();
+//            }
+//            $i++;
+//        }
+//
+//        if (!is_null($regionCrm)) {
+//            $entity->addRegion($regionCrm);
+//        }
+//        return $this;
+//    }
+
+//    /**
+//     * retirer la region crm
+//     * @param RegionUnifie $entity
+//     *
+//     * @return mixed
+//     */
+//    private function dissocierRegionCrm(RegionUnifie $entity)
+//    {
+//        foreach ($entity->getRegions() as $region) {
+//            if ($region->getSite()->getCrm() == 1) {
+////                $station->setStationUnifie(null);
+//                $entity->removeRegion($region);
+//                return $region;
+//            }
+//        }
+//        return false;
+//    }
+
+//    /**
+//     * Mettre à jours ou créer une nouvelle stationCrm (si elle n'existe pas)
+//     * Permet aussi la gestion des traductions si elles n'existent pas (notament dans le cas d'un ajout de langue)
+//     * Retourne vrai si elle est seulement mise à jours
+//     * Retourne faux s'il s'agit d'une nouvelle
+//     * @param RegionUnifie $regionUnifie
+//     * @param Region $regionCrm
+//     * @return bool
+//     */
+//    private function mettreAJourRegionCrm(RegionUnifie $regionUnifie, Region $regionCrm)
+//    {
+//        /** @var RegionTraduction $regionTraduc */
+//        $em = $this->getDoctrine()->getManager();
+//        $tabClassementSiteReferent = array();
+//
+////        récupère les classementReferent pour chaque site dans un tableau
+//        foreach ($regionUnifie->getRegions() as $region) {
+//            $tabClassementSiteReferent[] = $region->getSite()->getClassementReferent();
+//        }
+//
+//        // Récupèrer le site référent dans la base
+//        $siteReferent = $em->getRepository(Site::class)->findOneBy(array('classementReferent' => min($tabClassementSiteReferent)));
+//
+//        $langues = $em->getRepository(Langue::class)->findAll();
+//
+//        // Parcourir toutes les stations
+//        foreach ($regionUnifie->getRegions() as $region) {
+//
+//            // Si la site de la station est égale au site de référence
+//            if ($region->getSite() == $siteReferent) {
+////              ajouter les champs "communs"
+//                foreach ($langues as $langue) {
+////                    dump($langue);
+////                    recupere la traduction pour l'entite du site referent
+//                    $regionTraduc = $region->getTraductions()->filter(function (RegionTraduction $element) use ($langue) {
+//                        return $element->getLangue() == $langue;
+//                    })->first();
+//
+////                    récupère la traductin dans le crm
+//                    $regionTraducCrm = $regionCrm->getTraductions()->filter(function (RegionTraduction $element) use ($langue) {
+//                        return $element->getLangue() == $langue;
+//                    })->first();
+//
+//
+////                    null est interdit, si la traduction n'existe pas on passe les attributs a vide
+//                    if (is_null($regionTraduc->getLibelle())) {
+//                        $regionTraduc->setLibelle('');
+//                    }
+//                    if (is_null($regionTraduc->getDescription())) {
+//                        $regionTraduc->setDescription('');
+//                    }
+////                    Si la traduction n'existe pas dans le crm on creer une nouvelle traduction
+//                    if (empty($regionTraducCrm)) {
+//                        $regionTraducCrm = new RegionTraduction();
+//                        $regionTraducCrm->setRegion($regionCrm);
+//                        $regionTraducCrm->setLangue($langue);
+//                        //                    copie les attributs de traduction du site référent dans les traductions du crm
+//                        $regionTraducCrm->setLibelle($regionTraduc->getLibelle());
+//                        $regionTraducCrm->setDescription($regionTraduc->getDescription());
+//                        $regionCrm->addTraduction($regionTraducCrm);
+//                    } else {
+//                        //                    copie les attributs de traduction du site référent dans les traductions du crm
+//                        $regionTraducCrm->setLibelle($regionTraduc->getLibelle());
+//                        $regionTraducCrm->setDescription($regionTraduc->getDescription());
+//                    }
+//
+//                }
+//            } else {
+//
+////                permet de vérifier si la langue existe pour les sites non referents si elle n'existe pas on la rajoute
+//                foreach ($langues as $langue) {
+//
+////                    recupere la traduction pour la langue $langue
+//                    $regionTraduc = $region->getTraductions()->filter(function (RegionTraduction $element) use ($langue) {
+//                        return $element->getLangue() == $langue;
+//                    })->first();
+//
+////                    null est interdit, si la traduction n'existe pas on passe les attributs a vide
+//                    if (is_null($regionTraduc->getLibelle())) {
+//                        $regionTraduc->setLibelle('');
+//                    }
+//                    if (is_null($regionTraduc->getDescription())) {
+//                        $regionTraduc->setDescription('');
+//                    }
+//                }
+//            }
+//        }
+//    }
 
 }
