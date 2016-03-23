0c34aaf67680ba44a4f6d2b43d90fe227ee06e9e
Mathias <mathias@mondofute.com>
2016-03-23 16:46:46 +0100

    CRM-54 Station - Carte ID Formulaire Station OK StationID CRUD

---
+++ src/Mondofute/Bundle/StationBundle/Form/StationCarteIdentiteUnifieType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mondofute\Bundle\StationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StationCarteIdentiteUnifieType extends AbstractType
+{
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mondofute\Bundle\StationBundle\Entity\StationCarteIdentiteUnifie'
+        ));
+    }
+}
