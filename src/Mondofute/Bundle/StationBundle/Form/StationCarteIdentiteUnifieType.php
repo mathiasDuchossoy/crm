09b30fc1e5303eb2386534734c04b5bf637537dd
Mathias <mathias@mondofute.com>
2016-03-24 09:06:35 +0100

    CRM-54 Station - Carte ID newAction de Station

--- src/Mondofute/Bundle/StationBundle/Form/StationCarteIdentiteUnifieType.php
+++ src/Mondofute/Bundle/StationBundle/Form/StationCarteIdentiteUnifieType.php
@@ -3,11 +3,21 @@
 namespace Mondofute\Bundle\StationBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class StationCarteIdentiteUnifieType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('stationCarteIdentites', CollectionType::class, array('entry_type' => StationCarteIdentiteType::class));
+    }
 
     /**
      * @param OptionsResolver $resolver
@@ -15,7 +25,10 @@ class StationCarteIdentiteUnifieType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Mondofute\Bundle\StationBundle\Entity\StationCarteIdentiteUnifie'
+            'data_class' => 'Mondofute\Bundle\StationBundle\Entity\StationCarteIdentiteUnifie',
+            'locale' => 'fr_FR'
         ));
     }
+
+
 }
