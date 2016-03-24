e8d43562097e88cc32c3dd9d47f9ccfc276aafb2
Mathias <mathias@mondofute.com>
2016-03-24 15:05:49 +0100

    CRM-54 Station - Carte ID StationDescriptionUnifieController finis

---
+++ src/Mondofute/Bundle/StationBundle/Form/StationDescriptionTraductionType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mondofute\Bundle\StationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StationDescriptionTraductionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('accroche')
+            ->add('generalite')
+//            ->add('stationDescription')
+            ->add('langue', HiddenType::class, array('mapped' => false));
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mondofute\Bundle\StationBundle\Entity\StationDescriptionTraduction'
+        ));
+    }
+}
