34b5dff201945f14c2c110855c3130d7c258a7ee
Stephane <stephane@mondofute.com>
2016-04-01 15:46:40 +0200

    CRM-29 HebergementBundle ajout de la gestion des stations

--- src/Mondofute/Bundle/HebergementBundle/DependencyInjection/Configuration.php
+++ src/Mondofute/Bundle/HebergementBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-//        $rootNode = $treeBuilder->root('mondofute_hebergement');
+        $rootNode = $treeBuilder->root('mondofute_hebergement');
         $treeBuilder->root('mondofute_hebergement');
 
         // Here you should define the parameters that are allowed to
