ed9bb006833ecece548406a0b04722cbe0fa5b05
Stephane <stephane@mondofute.com>
2016-04-19 17:41:57 +0200

    CRM-29 HebergementBundle gestion des telfixes, telMobiles, adresse dans fournisseurHebergement

--- src/Mondofute/Bundle/HebergementBundle/Form/FournisseurHebergementType.php
+++ src/Mondofute/Bundle/HebergementBundle/Form/FournisseurHebergementType.php
@@ -3,6 +3,12 @@
 namespace Mondofute\Bundle\HebergementBundle\Form;
 
 use Mondofute\Bundle\FournisseurBundle\Entity\Fournisseur;
+use Nucleus\MoyenComBundle\Entity\Adresse;
+use Nucleus\MoyenComBundle\Entity\Fixe;
+use Nucleus\MoyenComBundle\Entity\Mobile;
+use Nucleus\MoyenComBundle\Form\AdresseType;
+use Nucleus\MoyenComBundle\Form\FixeType;
+use Nucleus\MoyenComBundle\Form\MobileType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,6 +28,9 @@ class FournisseurHebergementType extends AbstractType
 //            ->add('fournisseur', HiddenType::class, array('mapped' => false))
 //            ->add('hebergement', EntityType::class, array('class' => HebergementUnifie::class, 'choice_label' => 'id'))
             ->add('fournisseur', EntityType::class, array('class' => Fournisseur::class, 'choice_label' => 'enseigne'))
+            ->add('telFixe', FixeType::class, array('data_class' => Fixe::class))
+            ->add('telMobile', MobileType::class, array('data_class' => Mobile::class))
+            ->add('adresse', AdresseType::class, array('data_class' => Adresse::class))
         ;
     }
 
