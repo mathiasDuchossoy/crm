c10dd1b8a5c6b1ac6deb98617270a016eece2b91
Mathias <mathias@mondofute.com>
2016-05-17 17:05:27 +0200

    CRM-64 UtilisateurBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,7 +35,6 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Mondofute\Bundle\CoreBundle\MondofuteCoreBundle(),
             new \Mondofute\Bundle\ClientBundle\MondofuteClientBundle(),
-            new Sonata\IntlBundle\SonataIntlBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
