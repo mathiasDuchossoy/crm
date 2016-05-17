c7460e6a3c9e5d1ca17c3052ea74513581478969
Stephane <stephane@mondofute.com>
2016-05-17 09:38:27 +0200

    CRM-68 LogementBundle

---
+++ src/Mondofute/Bundle/LogementBundle/Controller/LogementUnifieController.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace Mondofute\Bundle\LogementBundle\Controller;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Mondofute\Bundle\LangueBundle\Entity\Langue;
+use Mondofute\Bundle\LogementBundle\Entity\Logement;
+use Mondofute\Bundle\LogementBundle\Entity\LogementTraduction;
+use Mondofute\Bundle\LogementBundle\Entity\LogementUnifie;
+use Mondofute\Bundle\LogementBundle\Form\LogementUnifieType;
+use Mondofute\Bundle\SiteBundle\Entity\Site;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * LogementUnifie controller.
+ *
+ */
+class LogementUnifieController extends Controller
+{
+    /**
+     * Lists all LogementUnifie entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $logementUnifies = $em->getRepository('MondofuteLogementBundle:LogementUnifie')->findAll();
+
+        return $this->render('@MondofuteLogement/logementunifie/index.html.twig', array(
+            'logementUnifies' => $logementUnifies,
+        ));
+    }
+
+    /**
+     * Creates a new LogementUnifie entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+//        Liste les sites dans l'ordre d'affichage
+        $sites = $em->getRepository(Site::class)->findBy(array(), array('classementAffichage' => 'asc'));
+        $langues = $em->getRepository(Langue::class)->findAll();
+
+        $sitesAEnregistrer = $request->get('sites');
+
+        $logementUnifie = new LogementUnifie();
+
+        $this->ajouterLogementsDansForm($logementUnifie);
+        $this->logementsSortByAffichage($logementUnifie);
+
+        $form = $this->createForm('Mondofute\Bundle\LogementBundle\Form\LogementUnifieType', $logementUnifie,
+            array('locale' => $request->getLocale()));
+        $form->add('submit', SubmitType::class, array(
+            'label' => 'Enregistrer',
+            'attr' => array('onclick' => 'copieNonPersonnalisable();remplirChampsVide();')
+        ));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($logementUnifie);
+            $em->flush();
+
+            return $this->redirectToRoute('logement_logement_show', array('id' => $logementUnifie->getId()));
+        }
+
+        return $this->render('@MondofuteLogement/logementunifie/new.html.twig', array(
+            'sitesAEnregistrer' => $sitesAEnregistrer,
+            'sites' => $sites,
+            'langues' => $langues,
+            'logementUnifie' => $logementUnifie,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Ajouter les logements qui n'ont pas encore été enregistré pour les sites existant, dans le formulaire
+     * @param LogementUnifie $entity
+     */
+    private function ajouterLogementsDansForm(LogementUnifie $entity)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $sites = $em->getRepository(Site::class)->findBy(array(), array('classementAffichage' => 'asc'));
+        $langues = $em->getRepository(Langue::class)->findAll();
+        foreach ($sites as $site) {
+            $siteExiste = false;
+            foreach ($entity->getLogements() as $logement) {
+                if ($logement->getSite() == $site) {
+                    $siteExiste = true;
+                    foreach ($langues as $langue) {
+
+//                        vérifie si $langue est présent dans les traductions sinon créé une nouvelle traduction pour l'ajouter à la région
+                        if ($logement->getTraductions()->filter(function (LogementTraduction $element) use ($langue) {
+                            return $element->getLangue() == $langue;
+                        })->isEmpty()
+                        ) {
+                            $traduction = new DepartementTraduction();
+                            $traduction->setLangue($langue);
+                            $logement->addTraduction($traduction);
+                        }
+                    }
+                }
+            }
+            if (!$siteExiste) {
+                $logement = new Logement();
+                $logement->setSite($site);
+
+                // ajout des traductions
+                foreach ($langues as $langue) {
+                    $traduction = new LogementTraduction();
+                    $traduction->setLangue($langue);
+                    $logement->addTraduction($traduction);
+                }
+                $entity->addLogement($logement);
+            }
+        }
+    }
+
+    /**
+     * Classe les departements par classementAffichage
+     * @param LogementUnifie $entity
+     */
+    private function logementsSortByAffichage(LogementUnifie $entity)
+    {
+        /** @var ArrayIterator $iterator */
+
+        // Trier les stations en fonction de leurs ordre d'affichage
+        $logements = $entity->getLogements(); // ArrayCollection data.
+
+        // Recueillir un itérateur de tableau.
+        $iterator = $logements->getIterator();
+        unset($departements);
+
+        // trier la nouvelle itération, en fonction de l'ordre d'affichage
+        $iterator->uasort(function (Logement $a, Logement $b) {
+            return ($a->getSite()->getClassementAffichage() < $b->getSite()->getClassementAffichage()) ? -1 : 1;
+        });
+
+        // passer le tableau trié dans une nouvelle collection
+        $logements = new ArrayCollection(iterator_to_array($iterator));
+        $this->traductionsSortByLangue($logements);
+
+        // remplacé les stations par ce nouveau tableau (une fonction 'set' a été créé dans Station unifié)
+        $entity->setLogements($logements);
+    }
+
+    /**
+     * Classe les traductions par rapport à leurs id
+     * @param $logements
+     */
+    private function traductionsSortByLangue($logements)
+    {
+        /** @var ArrayIterator $iterator */
+        /** @var Departement $departement */
+        foreach ($logements as $logement) {
+            $traductions = $logement->getTraductions();
+            $iterator = $traductions->getIterator();
+            // trier la nouvelle itération, en fonction de l'ordre d'affichage
+            $iterator->uasort(function (LogementTraduction $a, LogementTraduction $b) {
+                return ($a->getLangue()->getId() < $b->getLangue()->getId()) ? -1 : 1;
+            });
+
+            // passer le tableau trié dans une nouvelle collection
+            $traductions = new ArrayCollection(iterator_to_array($iterator));
+            $logement->setTraductions($traductions);
+        }
+    }
+
+    /**
+     * Finds and displays a LogementUnifie entity.
+     *
+     */
+    public function showAction(LogementUnifie $logementUnifie)
+    {
+        $deleteForm = $this->createDeleteForm($logementUnifie);
+
+        return $this->render('@MondofuteLogement/logementunifie/show.html.twig', array(
+            'logementUnifie' => $logementUnifie,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to delete a LogementUnifie entity.
+     *
+     * @param LogementUnifie $logementUnifie The LogementUnifie entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(LogementUnifie $logementUnifie)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('logement_logement_delete', array('id' => $logementUnifie->getId())))
+            ->setMethod('DELETE')
+            ->getForm();
+    }
+
+    /**
+     * Displays a form to edit an existing LogementUnifie entity.
+     *
+     */
+    public function editAction(Request $request, LogementUnifie $logementUnifie)
+    {
+        $deleteForm = $this->createDeleteForm($logementUnifie);
+        $editForm = $this->createForm('Mondofute\Bundle\LogementBundle\Form\LogementUnifieType', $logementUnifie);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($logementUnifie);
+            $em->flush();
+
+            return $this->redirectToRoute('logement_logement_edit', array('id' => $logementUnifie->getId()));
+        }
+
+        return $this->render('@MondofuteLogement/logementunifie/edit.html.twig', array(
+            'logementUnifie' => $logementUnifie,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a LogementUnifie entity.
+     *
+     */
+    public function deleteAction(Request $request, LogementUnifie $logementUnifie)
+    {
+        $form = $this->createDeleteForm($logementUnifie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($logementUnifie);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('logement_logement_index');
+    }
+}
---
+++ src/Mondofute/Bundle/LogementBundle/Entity/LogementTraduction.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Mondofute\Bundle\LogementBundle\Entity;
+
+/**
+ * LogementTraduction
+ */
+class LogementTraduction
+{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $nom;
+    /**
+     * @var string
+     */
+    private $descriptif;
+    /**
+     * @var \Mondofute\Bundle\LogementBundle\Entity\Logement
+     */
+    private $logement;
+    /**
+     * @var \Mondofute\Bundle\LangueBundle\Entity\Langue
+     */
+    private $langue;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return LogementTraduction
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get descriptif
+     *
+     * @return string
+     */
+    public function getDescriptif()
+    {
+        return $this->descriptif;
+    }
+
+    /**
+     * Set descriptif
+     *
+     * @param string $descriptif
+     *
+     * @return LogementTraduction
+     */
+    public function setDescriptif($descriptif)
+    {
+        $this->descriptif = $descriptif;
+
+        return $this;
+    }
+
+    /**
+     * Get logement
+     *
+     * @return \Mondofute\Bundle\LogementBundle\Entity\Logement
+     */
+    public function getLogement()
+    {
+        return $this->logement;
+    }
+
+    /**
+     * Set logement
+     *
+     * @param \Mondofute\Bundle\LogementBundle\Entity\Logement $logement
+     *
+     * @return LogementTraduction
+     */
+    public function setLogement(\Mondofute\Bundle\LogementBundle\Entity\Logement $logement = null)
+    {
+        $this->logement = $logement;
+
+        return $this;
+    }
+
+    /**
+     * Get langue
+     *
+     * @return \Mondofute\Bundle\LangueBundle\Entity\Langue
+     */
+    public function getLangue()
+    {
+        return $this->langue;
+    }
+
+    /**
+     * Set langue
+     *
+     * @param \Mondofute\Bundle\LangueBundle\Entity\Langue $langue
+     *
+     * @return LogementTraduction
+     */
+    public function setLangue(\Mondofute\Bundle\LangueBundle\Entity\Langue $langue = null)
+    {
+        $this->langue = $langue;
+
+        return $this;
+    }
+}
