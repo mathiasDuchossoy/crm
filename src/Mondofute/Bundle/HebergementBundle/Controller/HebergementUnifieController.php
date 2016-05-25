1d33fd9b7f7b6634532fa1bad4be21623e637928
Stephane <stephane@mondofute.com>
2016-05-25 09:47:38 +0200

    CRM-68 LogementBundle correction du libelle reception dans fiche hébergement

--- src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
+++ src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
@@ -387,8 +387,6 @@ class HebergementUnifieController extends Controller
                     $adresseSite->setCoordonneeGps(new CoordonneesGPS());
                     $hebergementSite->addMoyenCom($adresseSite);
                 }
-                dump($adresseSite);
-                die;
                 $adresseSite->setVille($adresse->getVille());
                 $adresseSite->setAdresse1($adresse->getAdresse1());
                 $adresseSite->setAdresse2($adresse->getAdresse2());
