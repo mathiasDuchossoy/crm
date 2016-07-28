ef6c308b742f6085048921e5e936d83e92a4d231
Mathias <mathias@mondofute.com>
2016-07-28 10:26:18 +0200

    CRM-83 Gestion Utilisateur mdp utilisateur

--- src/Mondofute/Bundle/ClientBundle/Form/ClientUserType.php
+++ src/Mondofute/Bundle/ClientBundle/Form/ClientUserType.php
@@ -5,7 +5,10 @@ namespace Mondofute\Bundle\ClientBundle\Form;
 use FOS\UserBundle\Util\LegacyFormHelper;
 use Mondofute\Bundle\ClientBundle\Entity\Client;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ClientUserType extends AbstractType
@@ -19,17 +22,33 @@ class ClientUserType extends AbstractType
         $builder
             ->add('client', ClientType::class, array(
                 'data_class' => Client::class
-            ))
+            ));
 //            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
 //            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
-            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
-                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
-                'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => 'form.password'),
-                'second_options' => array('label' => 'form.password_confirmation'),
-                'invalid_message' => 'fos_user.password.mismatch',
-            ))//            ->add('enabled')
-        ;
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $user = $event->getData();
+            $form = $event->getForm();
+
+            // vérifie si l'objet Product est "nouveau"
+            // Si aucune donnée n'est passée au formulaire, la donnée est "null".
+            // Ce doit être considéré comme un nouveau "Product"
+            if ($user && null !== $user->getId()) {
+                $form
+                    ->add('plainPassword', TextType::class, array(//                'mapped' => false
+                        'translation_domain' => 'FOSUserBundle',
+                        'label' => 'form.new_password',
+                        'required' => false
+                    ));
+            } else {
+                $form
+                    ->add('plainPassword', TextType::class, array(//                'mapped' => false
+                        'translation_domain' => 'FOSUserBundle',
+                        'label' => 'form.password',
+                    ));
+
+            }
+        });
+
     }
 
 
