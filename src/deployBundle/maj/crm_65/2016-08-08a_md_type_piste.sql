00f67fe99661bbfcd0e773a20d39467816c76aef
Mathias <mathias@mondofute.com>
2016-08-08 17:15:00 +0200

    rebase

---
+++ src/deployBundle/maj/crm_65/2016-08-08a_md_type_piste.sql
@@ -0,0 +1,9 @@
+INSERT INTO `type_piste` (`id`, `classement`) VALUES (1, 1);
+INSERT INTO `type_piste` (`id`, `classement`) VALUES (2, 2);
+INSERT INTO `type_piste` (`id`, `classement`) VALUES (3, 3);
+INSERT INTO `type_piste` (`id`, `classement`) VALUES (4, 4);
+
+INSERT INTO `type_piste_traduction` (`id`, `langue_id`, `type_piste_id`, `libelle`) VALUES (1, 1, 1, 'vertes');
+INSERT INTO `type_piste_traduction` (`id`, `langue_id`, `type_piste_id`, `libelle`) VALUES (2, 1, 2, 'bleues');
+INSERT INTO `type_piste_traduction` (`id`, `langue_id`, `type_piste_id`, `libelle`) VALUES (3, 1, 3, 'rouges');
+INSERT INTO `type_piste_traduction` (`id`, `langue_id`, `type_piste_id`, `libelle`) VALUES (4, 1, 4, 'noires');
