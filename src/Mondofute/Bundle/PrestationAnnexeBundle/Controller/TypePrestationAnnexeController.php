8e0bd5ab6673a67d6f2013bf729cefb23971d867
Mathias <mathias@mondofute.com>
2016-08-23 11:35:32 +0200

    updateSites

--- src/Mondofute/Bundle/PrestationAnnexeBundle/Controller/TypePrestationAnnexeController.php
+++ src/Mondofute/Bundle/PrestationAnnexeBundle/Controller/TypePrestationAnnexeController.php
@@ -2,7 +2,11 @@
 
 namespace Mondofute\Bundle\PrestationAnnexeBundle\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
 use Mondofute\Bundle\LangueBundle\Entity\Langue;
+use Mondofute\Bundle\PrestationAnnexeBundle\Entity\SousTypePrestationAnnexe;
+use Mondofute\Bundle\PrestationAnnexeBundle\Entity\SousTypePrestationAnnexeTraduction;
 use Mondofute\Bundle\PrestationAnnexeBundle\Entity\TypePrestationAnnexeTraduction;
 use Mondofute\Bundle\SiteBundle\Entity\Site;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -72,27 +76,73 @@ class TypePrestationAnnexeController extends Controller
     }
 
     /**
+     * Creates a form to delete a TypePrestationAnnexe entity.
+     *
+     * @param TypePrestationAnnexe $typePrestationAnnexe The TypePrestationAnnexe entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(TypePrestationAnnexe $typePrestationAnnexe)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('typeprestationannexe_delete', array('id' => $typePrestationAnnexe->getId())))
+            ->add('Supprimer', SubmitType::class, array('label' => 'supprimer', 'translation_domain' => 'messages'))
+            ->setMethod('DELETE')
+            ->getForm();
+    }
+
+    /**
      * Displays a form to edit an existing TypePrestationAnnexe entity.
      *
      */
     public function editAction(Request $request, TypePrestationAnnexe $typePrestationAnnexe)
     {
         /** @var Site $site */
-//        $deleteForm = $this->createDeleteForm($typePrestationAnnexe);
-        $em     = $this->getDoctrine()->getManager();
-        $langues    = $em->getRepository(Langue::class)->findAll();
+        $em = $this->getDoctrine()->getManager();
+        $langues = $em->getRepository(Langue::class)->findBy(array(), array('id' => 'ASC'));
+
+        foreach ($langues as $langue) {
+            $typePrestationAnnexeTraduction = $typePrestationAnnexe->getTraductions()->filter(function (TypePrestationAnnexeTraduction $element) use ($langue) {
+                return $element->getLangue() == $langue;
+            })->first();
+            if (false === $typePrestationAnnexeTraduction) {
+                $typePrestationAnnexeTraduction = new TypePrestationAnnexeTraduction();
+                $typePrestationAnnexeTraduction->setLangue($langue);
+                $typePrestationAnnexe->addTraduction($typePrestationAnnexeTraduction);
+            }
+        }
+
+        $originalSousTypePrestationAnnexes = new ArrayCollection();
+
+        // Create an ArrayCollection of the current SousTypePrestationAnnexe objects in the database
+        foreach ($typePrestationAnnexe->getSousTypePrestationAnnexes() as $sousTypePrestationAnnexe) {
+            $originalSousTypePrestationAnnexes->add($sousTypePrestationAnnexe);
+        }
 
         $editForm = $this->createForm('Mondofute\Bundle\PrestationAnnexeBundle\Form\TypePrestationAnnexeType', $typePrestationAnnexe);
         $editForm
             ->add('submit', SubmitType::class, array('label' => 'mettre.a.jour'));
         $editForm->handleRequest($request);
 
-//            dump($typePrestationAnnexe->getSousTypePrestationAnnexes()->first());die;
         if ($editForm->isSubmitted() && $editForm->isValid()) {
+            // remove the relationship between the tag and the Task
+            /** @var SousTypePrestationAnnexe $sousTypePrestationAnnexe */
+            foreach ($originalSousTypePrestationAnnexes as $sousTypePrestationAnnexe) {
+                if (false === $typePrestationAnnexe->getSousTypePrestationAnnexes()->contains($sousTypePrestationAnnexe)) {
+                    // if you wanted to delete the Tag entirely, you can also do that
+                    $em->remove($sousTypePrestationAnnexe);
+                }
+            }
+
+            // Récupération du controlleur de SousTypePrestationAnnexe;
+            $sousTypePrestationAnnexeController = new SousTypePrestationAnnexeController();
+            $sousTypePrestationAnnexeController->setContainer($this->container);
+
             $em->persist($typePrestationAnnexe);
             $em->flush();
 
-            $this->copieVerSites($typePrestationAnnexe);
+            $sites = $em->getRepository(Site::class)->findBy(array('crm' => 0));
+            $this->udpateSites($typePrestationAnnexe, $sites);
 
             return $this->redirectToRoute('typeprestationannexe_edit', array('id' => $typePrestationAnnexe->getId()));
         }
@@ -100,27 +150,75 @@ class TypePrestationAnnexeController extends Controller
         return $this->render('@MondofutePrestationAnnexe/typeprestationannexe/edit.html.twig', array(
             'typePrestationAnnexe'  => $typePrestationAnnexe,
             'form'                  => $editForm->createView(),
-//            'delete_form' => $deleteForm->createView(),
             'langues'                => $langues
         ));
     }
 
-    public function copieVerSites($typePrestationAnnexe){
+    public function udpateSites($typePrestationAnnexe, $sites)
+    {
+        /** @var SousTypePrestationAnnexe $sousTypePrestationAnnexe */
+        /** @var SousTypePrestationAnnexe $sousTypePrestationAnnexe */
         /** @var TypePrestationAnnexeTraduction $traductionSite */
         /** @var TypePrestationAnnexe $typePrestationAnnexeSite */
         /** @var TypePrestationAnnexe $typePrestationAnnexe */
         /** @var TypePrestationAnnexeTraduction $traduction */
-        $em     = $this->getDoctrine()->getManager();
-        $sites  = $em->getRepository(Site::class)->findBy(array('crm'=>0));
+        /** @var Site $site */
+        /** @var EntityManager $emSite */
         foreach ($sites as $site){
             $emSite  = $this->getDoctrine()->getManager($site->getLibelle());
             $typePrestationAnnexeSite = $emSite->find(TypePrestationAnnexe::class,$typePrestationAnnexe);
-            foreach ($typePrestationAnnexeSite->getTraductions() as $traductionSite){
-                $traduction = $typePrestationAnnexe->getTraductions()->filter(function (TypePrestationAnnexeTraduction $element) use ($traductionSite){
-                    return $element->getLangue()->getId() == $traductionSite->getId();
+
+            // modification des traductions du typePrestationAnnexe
+            foreach ($typePrestationAnnexe->getTraductions() as $traduction) {
+                $traductionSite = $typePrestationAnnexeSite->getTraductions()->filter(function (TypePrestationAnnexeTraduction $element) use ($traduction) {
+                    return $element->getLangue()->getId() == $traduction->getId();
                 })->first();
-                $traductionSite->setLibelle($traduction->getLibelle());
+                if (false === $traductionSite) {
+                    $traductionSite = new TypePrestationAnnexeTraduction();
+                    $typePrestationAnnexeSite->addTraduction($traductionSite);
+                    $traductionSite
+                        ->setLangue($emSite->find(Langue::class, $traduction->getLangue()));
+                }
+                $traductionSite
+                    ->setLibelle($traduction->getLibelle());
             }
+
+            // remove the relationship between the sousTypePrestationAnnexeSite and the typePrestationAnnexeSite
+            foreach ($typePrestationAnnexeSite->getSousTypePrestationAnnexes() as $sousTypePrestationAnnexeSite) {
+                $sousTypePrestationAnnexe = $typePrestationAnnexe->getSousTypePrestationAnnexes()->filter(function (SousTypePrestationAnnexe $element) use ($sousTypePrestationAnnexeSite) {
+                    return $element->getId() == $sousTypePrestationAnnexeSite->getId();
+                })->first();
+                if (false === $sousTypePrestationAnnexe) {
+                    // On doit le supprimer de l'entité parent
+                    $typePrestationAnnexeSite->removeSousTypePrestationAnnex($sousTypePrestationAnnexeSite);
+                    // if you wanted to delete the sousTypePrestationAnnexeSite entirely, you can also do that
+                    $emSite->remove($sousTypePrestationAnnexeSite);
+                }
+            }
+
+            foreach ($typePrestationAnnexe->getSousTypePrestationAnnexes() as $sousTypePrestationAnnexe) {
+                $sousTypePrestationAnnexeSite = $typePrestationAnnexeSite->getSousTypePrestationAnnexes()->filter(function (SousTypePrestationAnnexe $element) use ($sousTypePrestationAnnexe) {
+                    return $element->getId() == $sousTypePrestationAnnexe->getId();
+                })->first();
+                if (false === $sousTypePrestationAnnexeSite) {
+                    $sousTypePrestationAnnexeSite = new SousTypePrestationAnnexe();
+                    $typePrestationAnnexeSite->addSousTypePrestationAnnex($sousTypePrestationAnnexeSite);
+                }
+                foreach ($sousTypePrestationAnnexe->getTraductions() as $traduction) {
+                    $traductionSite = $sousTypePrestationAnnexeSite->getTraductions()->filter(function (SousTypePrestationAnnexeTraduction $element) use ($traduction) {
+                        return $element->getLangue()->getId() == $traduction->getLangue()->getId();
+                    })->first();
+                    if (false === $traductionSite) {
+                        $traductionSite = new SousTypePrestationAnnexeTraduction();
+                        $sousTypePrestationAnnexeSite->addTraduction($traductionSite);
+                        $traductionSite
+                            ->setLangue($emSite->find(Langue::class, $traduction->getLangue()));
+                    }
+                    $traductionSite
+                        ->setLibelle($traduction->getLibelle());
+                }
+            }
+
             $emSite->persist($typePrestationAnnexeSite);
             $emSite->flush();
         }
@@ -142,22 +240,5 @@ class TypePrestationAnnexeController extends Controller
         }
 
         return $this->redirectToRoute('typeprestationannexe_index');
-    }
-
-    /**
-     * Creates a form to delete a TypePrestationAnnexe entity.
-     *
-     * @param TypePrestationAnnexe $typePrestationAnnexe The TypePrestationAnnexe entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(TypePrestationAnnexe $typePrestationAnnexe)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('typeprestationannexe_delete', array('id' => $typePrestationAnnexe->getId())))
-            ->add('Supprimer', SubmitType::class, array('label' => 'supprimer', 'translation_domain' => 'messages'))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
     }
 }
