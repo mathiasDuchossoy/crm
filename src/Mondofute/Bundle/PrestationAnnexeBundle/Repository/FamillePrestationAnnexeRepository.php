b10781f96ff730641a7b6d79437599ce80918d33
Mathias <mathias@mondofute.com>
2016-08-26 16:12:41 +0200

    resolution conflits CRM-93 rebase preprod

--- src/Mondofute/Bundle/PrestationAnnexeBundle/Repository/FamillePrestationAnnexeRepository.php
+++ src/Mondofute/Bundle/PrestationAnnexeBundle/Repository/FamillePrestationAnnexeRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Mondofute\Bundle\PrestationAnnexeBundle\Repository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * FamillePrestationAnnexeRepository
@@ -10,4 +11,79 @@ namespace Mondofute\Bundle\PrestationAnnexeBundle\Repository;
  */
 class FamillePrestationAnnexeRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * @return mixed
+     */
+    public function countTotal()
+    {
+        return $this->createQueryBuilder('entity')
+            ->select('COUNT(entity)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    /**
+     * @param int $page
+     * @param $maxperpage
+     * @param $locale
+     * @param array $sortbyArray
+     * @param int $site
+     * @return Paginator
+     */
+    public function getList($page = 1, $maxperpage, $locale, $sortbyArray = array(), $site = 1)
+    {
+        $q = $this->createQueryBuilder('entity')
+            ->select('entity')
+            ->join('entity.traductions' , 'traductions')
+            ->join('traductions.langue' , 'langue')
+            ->where('langue.code = :locale')
+            ->setParameter('locale', $locale)
+            ->setFirstResult(($page - 1) * $maxperpage)
+            ->setMaxResults($maxperpage);
+
+        foreach ($sortbyArray as $key => $item) {
+            $q
+                ->orderBy($key, $item);
+        }
+
+        return new Paginator($q);
+    }
+
+
+    /**
+     * @param $locale
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    // récupérer les traductioin des départements crm qui sont de la langue locale
+    public function getTraductionsByLocale($locale)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('entity , traductions')
+            ->from('MondofutePrestationAnnexeBundle:FamillePrestationAnnexe', 'entity')
+            ->join('entity.traductions' , 'traductions')
+            ->join('traductions.langue' , 'langue')
+            ->where('langue.code = :locale')
+            ->setParameter('locale', $locale)
+            ->orderBy('traductions.libelle', 'ASC')
+        ;
+
+        return $qb;
+    }
+
+//    public function getTraductionsByLocale($locale)
+//    {
+//
+//        $qb = $this->getEntityManager()->createQueryBuilder();
+//        $qb->select('r , rt')
+//            ->from('MondofuteGeographieBundle:Departement', 'r')
+//            ->join('r.traductions', 'rt')
+//            ->join('r.site', 's')
+//            ->join('rt.langue', 'l')
+//            ->where("l.code = '$locale'");
+////        ->setParameter('code' , $locale)
+//        $qb->orderBy('r.id', 'ASC');
+//
+//        return $qb;
+//    }
+
 }
