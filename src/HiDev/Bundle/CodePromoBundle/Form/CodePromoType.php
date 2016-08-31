d3e81c5c229a4208fe7367ce16265cad4e2f0d89
Mathias <mathias@mondofute.com>
2016-08-31 10:58:51 +0200

    Validate en Validite

--- src/HiDev/Bundle/CodePromoBundle/Form/CodePromoType.php
+++ src/HiDev/Bundle/CodePromoBundle/Form/CodePromoType.php
@@ -50,8 +50,8 @@ class CodePromoType extends AbstractType
                 'label'         => 'Usage'
             ))
             ->add('actif')
-            ->add('codePromoPeriodeValidates', CollectionType::class, array(
-                    'entry_type' => 'HiDev\Bundle\CodePromoBundle\Form\CodePromoPeriodeValidateType',
+            ->add('codePromoPeriodeValidites', CollectionType::class, array(
+                    'entry_type' => 'HiDev\Bundle\CodePromoBundle\Form\CodePromoPeriodeValiditeType',
                     'allow_add' => true,
                     'allow_delete' => true,
                     'label' => 'Périodes de validité',
