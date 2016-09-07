8cbcc6f29c510223f2198c1e041e57d33c0ac637
Mathias <mathias@mondofute.com>
2016-09-07 19:25:01 +0200

    mise en place traductions et persistance sur les site pour fournisseurPrestationAnnexe

--- src/Mondofute/Bundle/FournisseurBundle/Entity/Fournisseur.php
+++ src/Mondofute/Bundle/FournisseurBundle/Entity/Fournisseur.php
@@ -35,6 +35,23 @@ class Fournisseur extends Moral
 {
     use FournisseurTrait;
 
+    private $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
     /**
      * @var \Doctrine\Common\Collections\Collection
      */
