0e2955884a51031c2bf689e4a00f2c2c459fe8c5
Mathias <mathias@mondofute.com>
2016-09-09 18:03:59 +0200

    modif bug 'supprimer' liste periode_validite

--- src/Mondofute/Bundle/FournisseurPrestationAnnexeBundle/Form/PeriodeValiditeType.php
+++ src/Mondofute/Bundle/FournisseurPrestationAnnexeBundle/Form/PeriodeValiditeType.php
@@ -3,6 +3,7 @@
 namespace Mondofute\Bundle\FournisseurPrestationAnnexeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,6 +18,28 @@ class PeriodeValiditeType extends AbstractType
         $builder
             ->add('dateDebut', 'datetime')
             ->add('dateFin', 'datetime')
+//            ->add('dateDebut', DateType::class, array(
+//                'required' => true,
+//                'widget' => 'single_text',
+//                'format' => 'dd/mm/yyyy hh:ii:ss',
+//                'attr' => array(
+//                    'class' => 'form-control input-inline datepicker date',
+////                    'data-provide' => 'datepicker-futur-tranche-cinq-ans',
+//                    'data-date-format' => 'dd/mm/yyyy hh:ii:ss',
+//                    'placeholder' => 'format_date',
+//                )
+//            ))
+//            ->add('dateFin', DateType::class, array(
+//                'required' => true,
+//                'widget' => 'single_text',
+//                'format' => 'dd/mm/yyyy hh:ii:ss',
+//                'attr' => array(
+//                    'class' => 'form-control input-inline datepicker date',
+////                    'data-provide' => 'datepicker-futur-tranche-cinq-ans',
+//                    'data-date-format' => 'dd/mm/yyyy hh:ii:ss',
+//                    'placeholder' => 'format_date',
+//                )
+//            ))
         ;
     }
     
