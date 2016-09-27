14604c9c3e74224648861665dab6610cb3cf7e55
Stephane <stephane@mondofute.com>
2016-09-27 14:18:37 +0200

    memoire

--- src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
+++ src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
@@ -1472,7 +1472,7 @@ class HebergementUnifieController extends Controller
         $this->chargerFournisseursStockslogementLocatif($fournisseurHebergements);
         $time = new \DateTime();
         echo $time->format('H:i:s');
-        echo memory_get_usage();
+//        echo memory_get_usage();
         die;
 //        dump(memory_get_usage());
 //        die;
