2364d2801f40e010ecddc54f23e6550bf6185a4a
Stephane <stephane@mondofute.com>
2016-09-29 13:04:04 +0200

    gestion mémoire

--- src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
+++ src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
@@ -1776,7 +1776,7 @@ class HebergementUnifieController extends Controller
 
     public function creerTableauxStocksHebergementPeriodeAction(Request $request, $idTypePeriode, $idHebergementUnifie)
     {
-//        ini_set('memory_limit','1G');
+        ini_set('memory_limit','1G');
 //        ini_set('max_execution_time',300);
 //        set_time_limit(300);
         $em = $this->getDoctrine()->getManager();
