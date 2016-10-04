fbcdecbff2b082b7dc31fc35076f37f63d3f555a
Stephane <stephane@mondofute.com>
2016-10-04 09:42:14 +0200

    memoire

--- src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
+++ src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
@@ -1862,7 +1862,6 @@ class HebergementUnifieController extends Controller
             $fournisseur[3] = $fournisseurHebergement->getFournisseur()->getId();
             array_push($data, $fournisseur);
         }
-        echo memory_get_peak_usage();
         return new JsonResponse($data);
     }
 
