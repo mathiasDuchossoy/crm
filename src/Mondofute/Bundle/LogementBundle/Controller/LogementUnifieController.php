158872f56d5ea4976c950ab4a362b573bceb446e
Stephane <stephane@mondofute.com>
2016-10-05 16:55:27 +0200

    memory

--- src/Mondofute/Bundle/LogementBundle/Controller/LogementUnifieController.php
+++ src/Mondofute/Bundle/LogementBundle/Controller/LogementUnifieController.php
@@ -669,7 +669,7 @@ class LogementUnifieController extends Controller
                 $memoryLimit = $memory;
                 break;
         }
-        $memoryLimitPourcentage=70;
+        $memoryLimitPourcentage=90;
         $logementsRef = $request->get('logements');
 //        dump($request->get('logements'));
 //        die;
