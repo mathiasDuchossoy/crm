d59d3a6e5b039d6fa4c531cce90fddb1bf2ba869
Mathias <mathias@mondofute.com>
2016-10-06 12:25:53 +0200

    creation des unite pour hebergement

---
+++ src/deployBundle/maj/crm_101/2016-09-01a_md_unite.sql
@@ -0,0 +1,7 @@
+INSERT INTO `unite` (`id`, `reference_id`, `multiplicateur_reference`, `discr`) VALUES (7, NULL, 1, 4);
+INSERT INTO `unite` (`id`, `reference_id`, `multiplicateur_reference`, `discr`) VALUES (8, NULL, 1, 4);
+
+INSERT INTO `unite_traduction` (`id`, `langue_id`, `unite_id`, `libelle`, `libelle_court`) VALUES (19, 2, 7, 'lutine', 'l');
+INSERT INTO `unite_traduction` (`id`, `langue_id`, `unite_id`, `libelle`, `libelle_court`) VALUES (21, 1, 7, 'lutin', 'l');
+INSERT INTO `unite_traduction` (`id`, `langue_id`, `unite_id`, `libelle`, `libelle_court`) VALUES (22, 2, 8, 'star', 's');
+INSERT INTO `unite_traduction` (`id`, `langue_id`, `unite_id`, `libelle`, `libelle_court`) VALUES (24, 1, 8, 'étoile', 'e');
