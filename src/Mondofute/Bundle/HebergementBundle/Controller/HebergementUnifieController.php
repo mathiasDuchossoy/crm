8166c49579519453656a2966e60b6bdc2e1a89a0
Stephane <stephane@mondofute.com>
2016-10-11 09:50:26 +0200

    ajout affichage erreur lors de la suppression

--- src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
+++ src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
@@ -1615,7 +1615,7 @@ class HebergementUnifieController extends Controller
             }
         } catch (ForeignKeyConstraintViolationException $except) {
             /** @var ForeignKeyConstraintViolationException $except */
-            dump($except->getMessage());
+            echo $except->getMessage();
             die;
             switch ($except->getCode()) {
                 case 0:
