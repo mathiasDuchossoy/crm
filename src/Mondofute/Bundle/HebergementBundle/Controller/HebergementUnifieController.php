c9a46bd8494d53ce81d3ba37143f9a6c4a16a3fa
Stephane <stephane@mondofute.com>
2016-10-11 09:47:57 +0200

    dump sur erreur de suppression d'un hébergement

--- src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
+++ src/Mondofute/Bundle/HebergementBundle/Controller/HebergementUnifieController.php
@@ -1615,6 +1615,8 @@ class HebergementUnifieController extends Controller
             }
         } catch (ForeignKeyConstraintViolationException $except) {
             /** @var ForeignKeyConstraintViolationException $except */
+            dump($except->getMessage());
+            die;
             switch ($except->getCode()) {
                 case 0:
                     $this->addFlash('error',
