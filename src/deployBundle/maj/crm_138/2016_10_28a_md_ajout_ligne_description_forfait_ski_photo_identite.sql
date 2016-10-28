5a68ce064161b614f7f1e05e956f272c6419d161
Mathias <mathias@mondofute.com>
2016-10-28 12:33:08 +0200

    maj listelancer

--- src/deployBundle/maj/crm_138/2016_10_28a_md_ajout_ligne_description_forfait_ski_photo_identite.sql
+++ src/deployBundle/maj/crm_138/2016_10_28a_md_ajout_ligne_description_forfait_ski_photo_identite.sql
@@ -1,3 +1,4 @@
-ALTER TABLE domaine ADD modele_description_forfait_ski_id INT UNSIGNED DEFAULT NULL;
-ALTER TABLE domaine ADD CONSTRAINT FK_78AF0ACC8B0F813C FOREIGN KEY (modele_description_forfait_ski_id) REFERENCES modele_description_forfait_ski (id);
-CREATE UNIQUE INDEX UNIQ_78AF0ACC8B0F813C ON domaine (modele_description_forfait_ski_id);
+INSERT INTO `ligne_description_forfait_ski` (`id`, `prix_id`, `age_min_id`, `age_max_id`, `categorie_id`, `present_id`, `quantite`, `classement`) VALUES (16, NULL, NULL, NULL, 2, NULL, 0, 16);
+
+INSERT INTO `ligne_description_forfait_ski_traduction` (`id`, `ligne_description_forfait_ski_id`, `langue_id`, `libelle`, `texte_dur`, `description`) VALUES (31, 16, 1, 'photo identité', '', '');
+INSERT INTO `ligne_description_forfait_ski_traduction` (`id`, `ligne_description_forfait_ski_id`, `langue_id`, `libelle`, `texte_dur`, `description`) VALUES (32, 16, 2, 'photo ID', '', '');
