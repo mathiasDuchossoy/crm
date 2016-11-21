38aee2277807677728d6d7a59ac7513efa0178fa
mathias <mathias@mondofute.com>
2016-11-21 16:34:37 +0100

    CRM-157 maj bdd fos-user

---
+++ src/deployBundle/maj/crm_157/2016_11_21a_md_maj_fos_user.sql
@@ -0,0 +1,3 @@
+ALTER TABLE interlocuteur_user DROP expired, DROP credentials_expired;
+ALTER TABLE utilisateur_user DROP expired, DROP credentials_expired;
+ALTER TABLE client_user DROP expired, DROP credentials_expired;
