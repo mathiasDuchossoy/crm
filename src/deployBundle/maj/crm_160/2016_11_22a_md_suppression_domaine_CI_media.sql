fc72b9a4f65e20680bdc13a78749e8ff9d4b1c70
mathias <mathias@mondofute.com>
2016-11-22 13:16:02 +0100

    CRM-160 domaine - erreur 500 => bug suppression corrigé

---
+++ src/deployBundle/maj/crm_160/2016_11_22a_md_suppression_domaine_CI_media.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS `domaine_carte_identite_image_traduction`;
+DROP TABLE IF EXISTS `domaine_carte_identite_image`;
+DROP TABLE IF EXISTS `domaine_carte_identite_photo_traduction`;
+DROP TABLE IF EXISTS `domaine_carte_identite_photo`;
