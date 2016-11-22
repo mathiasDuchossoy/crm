dbbf096e155172db3e071cafef8d4892791a93b4
mathias <mathias@mondofute.com>
2016-11-22 12:00:25 +0100

    CRM-155 logement - nb chambres

---
+++ src/Mondofute/Bundle/LogementBundle/Entity/NombreDeChambre.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mondofute\Bundle\LogementBundle\Entity;
+
+/**
+ * NombreDeChambre
+ */
+class NombreDeChambre
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
