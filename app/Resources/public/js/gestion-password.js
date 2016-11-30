81409793fa043cf817c2958e797149e9f27bb7db
mathias <mathias@mondofute.com>
2016-11-30 14:37:02 +0100

    CRM-172 User - mdp visu caché

--- app/Resources/public/js/gestion-password.js
+++ app/Resources/public/js/gestion-password.js
@@ -9,15 +9,19 @@ $(".password").each(function (index, element) {
 function displayInputPassword($element) {
     var visuPassword = '<button type="button" class="btn btn-default button-password-display" aria-label="Left Align"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>';
     var $visuPassword = $(visuPassword);
-    if ($element.parent().find('button.button-password-display')[0] == null) {
+    var $button = $element.parent().find('button.button-password-display')[0];
+    if ($button == null) {
         $visuPassword.insertAfter($element);
-        $visuPassword.on('mousedown', function () {
-            $element.prop('type', "text");
-        });
-        $visuPassword.on('mouseup', function () {
-            $element.prop('type', "password");
-        });
     }
+    else {
+        $visuPassword = $($button);
+    }
+    $visuPassword.on('mousedown', function () {
+        $element.prop('type', "text");
+    });
+    $visuPassword.on('mouseup', function () {
+        $element.prop('type', "password");
+    });
 }
 
 $(document).bind('DOMNodeInserted', function (event) {
