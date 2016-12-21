279c1c1187c43ec0da63c31f47fd20dfbbbb00d5
mathias <mathias@mondofute.com>
2016-12-21 12:28:47 +0100

    DecoteBundle en copie exact de promotionBundle

---
+++ src/Mondofute/Bundle/DecoteBundle/Entity/PromotionHebergement.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Mondofute\Bundle\PromotionBundle\Entity;
+
+use Mondofute\Bundle\FournisseurBundle\Entity\Fournisseur;
+use Mondofute\Bundle\HebergementBundle\Entity\Hebergement;
+
+/**
+ * PromotionHebergement
+ */
+class PromotionHebergement
+{
+    /**
+     * @var Hebergement
+     */
+    private $hebergement;
+    /**
+     * @var Fournisseur
+     */
+    private $fournisseur;
+    /**
+     * @var Promotion
+     */
+    private $promotion;
+
+    /**
+     * Get hebergement
+     *
+     * @return Hebergement
+     */
+    public function getHebergement()
+    {
+        return $this->hebergement;
+    }
+
+    /**
+     * Set hebergement
+     *
+     * @param Hebergement $hebergement
+     *
+     * @return PromotionHebergement
+     */
+    public function setHebergement(Hebergement $hebergement = null)
+    {
+        $this->hebergement = $hebergement;
+
+        return $this;
+    }
+
+    /**
+     * Get fournisseur
+     *
+     * @return Fournisseur
+     */
+    public function getFournisseur()
+    {
+        return $this->fournisseur;
+    }
+
+    /**
+     * Set fournisseur
+     *
+     * @param Fournisseur $fournisseur
+     *
+     * @return PromotionHebergement
+     */
+    public function setFournisseur(Fournisseur $fournisseur = null)
+    {
+        $this->fournisseur = $fournisseur;
+
+        return $this;
+    }
+
+    /**
+     * Get promotion
+     *
+     * @return Promotion
+     */
+    public function getPromotion()
+    {
+        return $this->promotion;
+    }
+
+    /**
+     * Set promotion
+     *
+     * @param Promotion $promotion
+     *
+     * @return PromotionHebergement
+     */
+    public function setPromotion(Promotion $promotion = null)
+    {
+        $this->promotion = $promotion;
+
+        return $this;
+    }
+}
---
+++ src/Mondofute/Bundle/DecoteBundle/Form/PromotionPeriodeValiditeDateType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Mondofute\Bundle\PromotionBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PromotionPeriodeValiditeDateType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('dateDebut', DateTimeType::class,
+                array(
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd/MM/yyyy - HH:mm',//yyyy-MM-dd'T'HH:mm:ssZZZZZ
+                    'model_timezone' => 'EUROPE/Paris',
+                    'attr' => array(
+//                        'class' => 'form-control input-inline datetimepicker datetime',
+                        'class' => 'datetimepicker',
+                        'data-date-format' => 'dd/MM/yyyy HH:mm',
+                        'placeholder' => 'jj/mm/aaaa - hh:mm',
+                    ),
+                ))
+            ->add('dateFin', DateTimeType::class,
+                array(
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd/MM/yyyy - HH:mm',//yyyy-MM-dd'T'HH:mm:ssZZZZZ
+                    'model_timezone' => 'EUROPE/Paris',
+                    'attr' => array(
+//                        'class' => 'form-control input-inline datetimepicker datetime',
+                        'class' => 'datetimepicker',
+                        'data-date-format' => 'dd/MM/yyyy HH:mm',
+//                        'placeholder' => 'format_date',
+                        'placeholder' => 'jj/mm/aaaa - hh:mm',
+                    )
+                ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mondofute\Bundle\PromotionBundle\Entity\PromotionPeriodeValiditeDate'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mondofute_bundle_promotionbundle_promotionperiodevaliditedate';
+    }
+
+
+}
